53d464f6c66be6c6747f4af2903d173f43aa1187
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-05-08 11:15:39 +0700

    feat: bingo gameplay

---
+++ src/app/gameplay/mil-bingo/module/[moduleId]/[bingoCardId]/page.tsx
@@ -0,0 +1,10 @@
+import BingoCardClientWrapper from './client-wrapper';
+
+export default function BingoCardPage() {
+  return (
+    <div className="relative min-h-screen bg-[#fcefd4] flex items-center justify-center px-4 py-8">
+      <img src="/MIL BINGO.png" alt="MIL BINGO" className="absolute top-0 left-1/2 -translate-x-1/2 h-30" />
+      <BingoCardClientWrapper />
+    </div>
+  );
+}
